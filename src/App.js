05613fa93141a599b2a1e5c3c8240a70c7680d52
Thao <levunhatthao05@gmail.com>
2022-08-25 18:55:12 +0300

    setting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+ 
+function App() {
+  return (
+    <div className="App">
+      <h1>App css</h1>
+    </div>
+  );
+}
+
+export default App;
